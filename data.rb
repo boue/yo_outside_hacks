ec9122003abfa59a3bb78e09a7d43bc5dedf4f0b
Germain Boue <boue.germain@gmail.com>
2014-07-26 17:53:29 -0700

    At the moment I am able to access each PAGE_URL for individual artists and Nokogiri is able to load the page. The challenge lays in now accessing the exact data we are looking for which includes a date and a time.

--- data.rb
+++ data.rb
@@ -7,10 +7,18 @@ require 'nokogiri'
 json = File.read("data.json")
 parsed = JSON.parse(json)
 
-doc = Nokogiri::HTML(open("http://www.threescompany.com/"))
+# doc = Nokogiri::HTML(open("http://www.threescompany.com/"))
 
 parsed.each do |artist|
-	pp artist["link"]
+	PAGE_URL = artist["link"]
+	pp PAGE_URL
+	page = Nokogiri::HTML(open(PAGE_URL))
+	pp page
+	# PAGE_URL = Nokogiri::HTML(open(artist["link"]))
+	# puts page.css("title").text 
+	# time = PAGE_URL.css('ds-arrange ds-performance-location href').text
+	# pp time
+# 	pp artist["link"]
 end
 
 #STEP 1: PARSE THE SET TIMES 
