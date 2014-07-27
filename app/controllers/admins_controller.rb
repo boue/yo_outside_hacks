4fe778dc9d275982cda3cb940a4b9aac2e1441e9
Germain Boue <boue.germain@gmail.com>
2014-07-27 03:03:04 -0700

    Made the form for admin login

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,12 +2,12 @@ class AdminsController < ApplicationController
   
   def index
   	#renders page with log in box
-  	if admin.logged_in?
+  	# if admin.logged_in?
 
-  	else 
-  		flash[:notice] = "Invalid Admin username/password combination"
-  		redirect_to admin_path
-  	end
+  	# else 
+  	# 	flash[:notice] = "Invalid Admin username/password combination"
+  	# 	redirect_to admin_path
+  	# end
   end
 
 
