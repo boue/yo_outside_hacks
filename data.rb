5ced089270cc546c1c4ce6f7a09979718482824e
Germain Boue <boue.germain@gmail.com>
2014-07-26 18:14:24 -0700

    Figured out the CSS exact location able to extract date and time, regex possible for time

--- data.rb
+++ data.rb
@@ -10,10 +10,10 @@ parsed = JSON.parse(json)
 # doc = Nokogiri::HTML(open("http://www.threescompany.com/"))
 
 parsed.each do |artist|
-	PAGE_URL = artist["link"]
-	pp PAGE_URL
-	page = Nokogiri::HTML(open(PAGE_URL))
-	pp page
+	page_url = artist["link"]
+	pp page_url
+	page = Nokogiri::HTML(open(page_url))
+	pp page.css('html body.ds-not-logged-in.ds-band-show-page div#page div#ds-content-outer-wrapper div#ds-content-wrapper div#ds-content-inner-wrapper div#ds-content div.ds-header div.ds-arrange.ds-performance-location p a').text
 	# PAGE_URL = Nokogiri::HTML(open(artist["link"]))
 	# puts page.css("title").text 
 	# time = PAGE_URL.css('ds-arrange ds-performance-location href').text
