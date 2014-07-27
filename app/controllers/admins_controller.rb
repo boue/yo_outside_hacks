60abec86a55027f5685c6c55915bc809b7bfb143
Germain Boue <boue.germain@gmail.com>
2014-07-27 02:11:05 -0700

    Set two different actions for admin
    - First action lands to where admin logs in
    - Second action allows admin to yo to one user or yo to all

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,11 +1,16 @@
 class AdminsController < ApplicationController
   
   def index
-  
+  	#renders page with log in box 
   end
 
   def yo
-
+  	admin.logged_in?
+	  	if params[:type] == "one" && params[:username]
+	  		#send to one user
+	  	elsif params[:type] == "all" 
+	  		#render all page with button to send to all 
+  		end	
   end
 
 end
