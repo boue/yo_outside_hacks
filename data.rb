79254ecfafe957dac856547cc692fb81609c73ed
Germain Boue <boue.germain@gmail.com>
2014-07-26 21:00:27 -0700

    The hashes used to add but its not permanent since I am dealing with an array of hashes, doing .each is not permanently making a change, need to store changes as new_data and write to new file

--- data.rb
+++ data.rb
@@ -12,23 +12,32 @@ require 'date'
 require 'debugger'
 
 json = File.read("data.json")
-
 parsed = JSON.parse(json)
 
+#method that adds a new key, value pair
 def hash_add(hash, new_key, new_value)
   temp_hash = {}
   temp_hash[new_key] = new_value
   temp_hash.merge!(hash)
   hash = temp_hash
-  puts hash
 end
 
+#final data that is to be written to new file
+new_data = [] 
 
+
+#iteration that adds two new key-value pairs which are start_time and end_time to JSON
+parsed.each do |artist|
+debugger
+	#adds the start time 
+	hash_add(artist["shows"][0], "start_time", artist["shows"][0]["time"][0..3] )
+debugger
+	#adds the end time
+	# hash_add(artist["shows"][0], "end_time", artist["shows"][0]["time"][7..10] )
+end
+
+#Open file new_data.json and write to it changes
 File.open("new_data.json", 'w') do |file|
-	parsed.each do |artist|
-		hash_add(artist["shows"][0], "start_time", artist["shows"][0]["time"][0..3] )
-		hash_add(artist["shows"][0], "end_time", artist["shows"][0]["time"][7..10] )
-	end
-	file.write(parsed)
+	file << new_data
 end
 
