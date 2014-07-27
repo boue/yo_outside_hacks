d0f967f45e7c9d303f87a18fa5bb83e4959ebff5
Germain Boue <boue.germain@gmail.com>
2014-07-26 22:20:51 -0700

    Created .env file and added it to git ignore so it doesn't push up to github

--- app/controllers/yos_controller.rb
+++ app/controllers/yos_controller.rb
@@ -2,8 +2,9 @@ class YosController < ApplicationController
 
 	def yo
 		if params[:api_key] == ENV[API_KEY]
+			puts "YOU ARE IN"
 			# .env file with key, value pairs in it
-			c1528fbba492bffb57aae12a8abfa0c80e671939b5040fa4a0145101b96e8e9c
+		end
 		# @yos = Yo.find(20)
 	end
 
