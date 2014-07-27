1203ed21ba63a9ea36e9c23e5828164d291fc867
Germain Boue <boue.germain@gmail.com>
2014-07-26 22:10:45 -0700

    Got to check if the api_key matches with environment secret key then you can give the firebase access to load all yo's

--- app/controllers/yos_controller.rb
+++ app/controllers/yos_controller.rb
@@ -1,3 +1,10 @@
 class YosController < ApplicationController
 
+	def yo
+		if params[:api_key] == ENV[API_KEY]
+			# .env file with key, value pairs in it
+			c1528fbba492bffb57aae12a8abfa0c80e671939b5040fa4a0145101b96e8e9c
+		# @yos = Yo.find(20)
+	end
+
 end
