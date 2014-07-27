5d67a8b485281d181262f5884f17db9f8cfddc37
Germain Boue <boue.germain@gmail.com>
2014-07-26 22:56:29 -0700

    Small diff

--- app/controllers/yos_controller.rb
+++ app/controllers/yos_controller.rb
@@ -2,7 +2,7 @@ class YosController < ApplicationController
 
 	def yo
 		if params[:api_key] == ENV["API_KEY"]
-			p "YOU ARE IN"
+			#firebase in there 
 			# .env file with key, value pairs in it
 		end
 		# @yos = Yo.find(20)
