4fd3d737dda9b58812dab7c0ef70cf1a08b8c37a
Germain Boue <boue.germain@gmail.com>
2014-07-27 01:46:18 -0700

    Firebase is set as a constant
    	- FIREBASE located in /config/initializers/firebase.rb
    	- Makes the app way safer

--- app/controllers/yos_controller.rb
+++ app/controllers/yos_controller.rb
@@ -1,13 +1,11 @@
 class YosController < ApplicationController
 	
 	def yo
-		base_uri = 'https://yo-outside-hacks.firebaseio.com/'
-		firebase = Firebase::Client.new(base_uri)
 		if params[:api_key] == ENV["API_KEY"]
-			firebase.push("yos", { :username => params[:username], :timestamp => Time.now })
-			counter = firebase.get("counter").body
+			FIREBASE.push("yos", { :username => params[:username], :timestamp => Time.now })
+			counter = FIREBASE.get("counter").body
 			counter = counter ? (counter + 1) : 1
-			firebase.set("counter", counter)
+			FIREBASE.set("counter", counter)
 		end
 		redirect_to root_path
 	end
