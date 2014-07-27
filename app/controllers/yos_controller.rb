23bb31fe693e51589e711ed997dbfeb2744b4531
Germain Boue <boue.germain@gmail.com>
2014-07-26 23:52:59 -0700

    Cleaned up code

--- app/controllers/yos_controller.rb
+++ app/controllers/yos_controller.rb
@@ -1,17 +1,12 @@
 class YosController < ApplicationController
 	
-	
 	def yo
 		base_uri = 'https://yo-outside-hacks.firebaseio.com/'
 		firebase = Firebase::Client.new(base_uri)
 		if params[:api_key] == ENV["API_KEY"]
 			response = firebase.push("yos", { :username => params[:username], :timestamp => Time.now })
-			p response 
-			#firebase in there 
-			# .env file with key, value pairs in it
 		end
 		redirect_to root_path
-		# @yos = Yo.find(20)
 	end
 
 end
