b0bd1af5e681e8c21f5e32e95c375a8e8ac3d450
Germain Boue <boue.germain@gmail.com>
2014-07-26 20:12:51 -0700

    Able to add start_time and end_time have to see if it permanently makes changes to actual hash

--- data.rb
+++ data.rb
@@ -1,36 +1,34 @@
+#1) extract start time and end time separately and store in variable for each artist with "start_time": "8:05" and "end_time" : "9:50"
+#2) add in original "shows" hash 
+#3) save in a new file 
+# start_time = []
+# end_time = []
+
 require 'rubygems'
 require 'json'
 require 'pp'
 require 'open-uri'
-require 'nokogiri'
 require 'date'
 require 'debugger'
 
 json = File.read("data.json")
+
 parsed = JSON.parse(json)
 
-# doc = Nokogiri::HTML(open("http://www.threescompany.com/"))
-
-parsed.each do |artist|
-	page_url = artist["link"]
-	pp page_url
-	page = Nokogiri::HTML(open(page_url))
-	# show_time = page.css('html body.ds-not-logged-in.ds-band-show-page div#page div#ds-content-outer-wrapper div#ds-content-wrapper div#ds-content-inner-wrapper div#ds-content div.ds-header div.ds-arrange.ds-performance-location p a').text
-	stage_name = page.css('html body.ds-not-logged-in.ds-band-show-page div#page div#ds-content-outer-wrapper div#ds-content-wrapper div#ds-content-inner-wrapper div#ds-content div.ds-header div.ds-arrange.ds-performance-location p').text
-	debugger
-	pp stage_name
-	# show_time_hour = page.css('html body.ds-not-logged-in.ds-band-show-page div#page div#ds-content-outer-wrapper div#ds-content-wrapper div#ds-content-inner-wrapper div#ds-content div.ds-header div.ds-arrange.ds-performance-location p a').text.gsub(//)
-
-	# pp show_time
-	# PAGE_URL = Nokogiri::HTML(open(artist["link"]))
-	# puts page.css("title").text 
-	# time = PAGE_URL.css('ds-arrange ds-performance-location href').text
-	# pp time
-# 	pp artist["link"]
+def hash_add(hash, new_key, new_value)
+  temp_hash = {}
+  temp_hash[new_key] = new_value
+  temp_hash.merge!(hash)
+  hash = temp_hash
+  puts hash
 end
 
-#STEP 1: PARSE THE SET TIMES 
-#STEP 2: LINK THEM TO ARTIST
-#STEP 3: ADD THEM TO JSON AND SAVE 
 
-#pp parsed[0] - one artist 
+File.open("new_data.json", 'w') do |file|
+	parsed.each do |artist|
+		hash_add(artist["shows"][0], "start_time", artist["shows"][0]["time"][0..3] )
+		hash_add(artist["shows"][0], "end_time", artist["shows"][0]["time"][7..10] )
+	end
+	file.write(parsed)
+end
+
