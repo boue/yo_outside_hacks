a480cd431a47f97396e19401b205c7e062f49a63
Germain Boue <boue.germain@gmail.com>
2014-07-27 01:59:58 -0700

    Added routes and actions for admin
    	- 2 actions index and yo
    	- 2 routes for admin

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,11 @@
+class AdminsController < ApplicationController
+  
+  def index
+  
+  end
+
+  def yo
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
   get 'yo/:api_key', to: 'yos#yo'
+  get '/admin', to: 'admins#index'
+  get '/admin/yo', to: 'admins#yo'
   root 'welcome#index'
+  
 end
