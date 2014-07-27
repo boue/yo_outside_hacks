c3e3c3390ffb20944a4dbbe1934a8cf5d075dad7
Steven Miyakawa SAM <samprofessional@gmail.com>
2014-07-27 07:59:32 -0700

    Make sure only necessary JS is loaded on each page

--- app/assets/javascripts/yo.js
+++ app/assets/javascripts/yo.js
@@ -1,40 +1,45 @@
 $(function() {
-  var ref = new Firebase("https://YO-OUTSIDE-HACKS.firebaseio.com/");
-  var yosRef = ref.child("yos");
-  var $btnText = $('span');
 
-  // initial load
-  yosRef.on("child_added", function (yo) {
-    var yo = yo.val();
-    var $yoNode = $('<li>').text(yo.username);
+  var $body = $('body');
 
-    $('ul').prepend($yoNode);
-    $yoNode.slideDown();
-  });
+  if ( $body.hasClass('welcome') ) {
+    var ref = new Firebase("https://YO-OUTSIDE-HACKS.firebaseio.com/");
+    var yosRef = ref.child("yos");
+    var addAnimations = function() {
+      $body.addClass('animate');
+      $('li').addClass('dont-animate');
+    }
 
-  $("#yo-all").click(function() {
-    toggleButton();
-    $.ajax({
-      url: '/yo_send',
-      type: 'POST',
-      data: { type: 'all' }
-    }).done(function(){
-      toggleButton();
-      $btnText.text('SENT!')
-      setTimeout(function() { $btnText.text('YO ALL!') }, 1000)
+    // initial load
+    yosRef.on("child_added", function (yo) {
+      var yo = yo.val();
+      var $yoNode = $('<li>').text(yo.username);
+
+      $('ul').prepend($yoNode);
+      $yoNode.slideDown();
     });
-  });
 
-  // start animating the addition
-  setTimeout(addAnimations, 2000);
-
-  function addAnimations() {
-    $('body').addClass('animate');
-    $('li').addClass('dont-animate');
+    // start animating the addition
+    setTimeout(addAnimations, 2000);
   }
+  else if ( $body.hasClass('admins') ) {
+    var $btnText = $('span');
+    var toggleButton = function() {
+      $('img').toggle();
+      $('span').toggle();
+    }
 
-  function toggleButton() {
-    $('img').toggle();
-    $btnText.toggle();
+    $("#yo-all").click(function() {
+      toggleButton();
+      $.ajax({
+        url: '/yo_send',
+        type: 'POST',
+        data: { type: 'all' }
+      }).done(function(){
+        toggleButton();
+        $btnText.text('SENT!')
+        setTimeout(function() { $btnText.text('YO ALL!') }, 1000)
+      });
+    });
   }
 });
